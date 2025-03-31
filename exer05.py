9410e9c4df6160bec191df4227f25d3305e86837
narely <narelyalbuquerque25@gmail.com>
2025-03-31 16:59:38 -0300

    recuperacao

--- exer05.py
+++ exer05.py
@@ -2,7 +2,9 @@ nota1=float(input("informe a primeira nota"))
 nota2=float(input("informe a ssegunda nota"))
 nota3=float(input("informe a terceira nota"))
 media=(nota1+nota2+nota3)/3
-if media>7:
+if media>=7:
     print(f"aprovado")
+elif media >= 4:
+    print(f"recuperacao")
 else:
     print(f"reprovado")
