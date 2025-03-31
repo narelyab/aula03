ce3ab59956793ce5ed341c693256f186c1a56b6d
narely <narelyalbuquerque25@gmail.com>
2025-03-31 14:17:31 -0300

    adicinar nome idade e salario

---
+++ exer01.py
@@ -0,0 +1,5 @@
+nome=input("qual e o seu nome?\n")
+idade=int(input("qual sua idade?\n"))
+salario=float(input("qual o salario?\n"))
+print(f"ola  {nomee}  minha idade e  {idade}  eu recebo  {salario:.2fnarely}")
+
