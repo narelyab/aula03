d989baece5d32a39dc190d441e00924f8c821d63
narely <narelyalbuquerque25@gmail.com>
2025-03-31 16:14:41 -0300

    if,else

---
+++ exer04.py
@@ -0,0 +1,7 @@
+numero1=float(input("me informe um numero\n"))
+numero2=float(input("me informe um numero\n"))
+if numero1>numero2:
+    print(numero2,numero1)
+else:
+    print(numero1,numero2)
+10
