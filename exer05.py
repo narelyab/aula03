1eb4dc1b8d4989f50dd52aeb3b8e8c9f6b2e0b13
narely <narelyalbuquerque25@gmail.com>
2025-03-31 16:41:05 -0300

    aprovacao ou reprovacao do aluno

---
+++ exer05.py
@@ -0,0 +1,8 @@
+nota1=float(input("informe a primeira nota"))
+nota2=float(input("informe a ssegunda nota"))
+nota3=float(input("informe a terceira nota"))
+media=(nota1+nota2+nota3)/3
+if media>7:
+    print(f"aprovado")
+else:
+    print(f"reprovado")
