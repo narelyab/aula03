c088b03426a1976235b4fb11b29b2009546b3859
narely <narelyalbuquerque25@gmail.com>
2025-03-31 14:48:35 -0300

    aumento de salario

--- exer01.py
+++ exer01.py
@@ -1,5 +1,7 @@
 nome=input("qual e o seu nome?\n")
 idade=int(input("qual sua idade?\n"))
 salario=float(input("qual o salario?\n"))
-print(f"ola  {nomee}  minha idade e  {idade}  eu recebo  {salario:.2fnarely}")
-
+percentual=float(input("digite o percentual de aumento"))
+valorp=salario * percentual/100
+novosalario=salario+valorp
+print(f"ola  {nomee}  minha idade e  {idade}  eu recebo  {salario:.2f}  mais tive um aumento de {valorp},entao no final irei receber {novosalario}")
