cab9d0283c7c53fd1c088ac3d06419b3954f6490
narely <narelyalbuquerque25@gmail.com>
2025-04-02 16:58:41 -0300

    hora 6:05

---
+++ exer09.py
@@ -0,0 +1,21 @@
+hora1=float(input("que horas sao?"))
+minuto1=float(input("e quantos minutos?"))
+hora2=float(input("que horas sao ?"))
+minuto2=float(input("e quantos minutos?"))
+
+saidahora = (hora1+hora2-12)
+saidaminutos = minuto1+minuto2
+if saidaminutos >=60:
+    saidahora <= 1.05
+    print(saidaminutos)
+    print(saidahora)
+
+
+
+
+
+
+
+
+
+
